fa617bcbd938e7395227c7e640db7446e9402777
s21arvesh <sarveshmestry01@gmail.com>
2026-05-21 14:50:29 +0530

    changes in the settings file

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# ADD THIS LINE: This tells Django where to copy static files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
