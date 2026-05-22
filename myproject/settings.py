9f22c3be824142dd8ba9885cf77fe57ae6205009
s21arvesh <sarveshmestry01@gmail.com>
2026-05-22 14:15:55 +0530

    settings command

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-n6yo73cmy#d(c6)igj)y_p*!&1=w_^3rgf=c(-2999_-jl720a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ec2 public ip
 ALLOWED_HOSTS = ['32.196.230.61', 'localhost', '127.0.0.1']
 
 
